f41180399e924a5dfd7707d8fa4342a99e0efe75
leovt <leonhard.vogt@gmx.ch>
2019-08-15 22:27:58 +0200

    moved method to cli_game

--- state.py
+++ state.py
@@ -527,37 +527,6 @@ def draw_card(player):
     else:
         yield DrawEmptyEvent(player.name)
 
-def print_player_view(view):
-    print('=' * 80)
-    print(f'It is the {view.step} of {view.players[view.active_player].name}s turn.')
-    print('Players:')
-    for p in view.players.values():
-        print(f'    {p.name}: {p.life} life; {p.hand_size} cards in hand, {p.energy_pool} in pool')
-        if p.graveyard:
-            print('        Graveyard:')
-            for card in p.graveyard:
-                print(f'            {card.name}')
-    print('-' * 80)
-    if view.stack:
-        print('Stack')
-        for item in reversed(view.stack):
-            print(f'    {item}')
-        print('-' * 80)
-    print('Battlefield')
-    for pid, p in view.players.items():
-        print('    Controlled by', p.name)
-        nothing = True
-        for obj in view.battlefield.values():
-            if obj.controller == pid:
-                print('       ', obj.name)
-                nothing = False
-        if nothing:
-            print('        nothing')
-    print('-' * 80)
-    print('Your hand:')
-    for card in view.you.hand:
-        print('   ', card.name)
-
 def can_play_source(player):
     return True
 
