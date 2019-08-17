0c900011425ff889fb33c143600a31e4e4cff69c
leovt <leonhard.vogt@gmx.ch>
2019-08-17 20:40:40 +0200

    moved login and user management to new app structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import UserMixin, LoginManager, current_user, login_user, logout_user, login_required, current_user
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='client')
+
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+login_mgr = LoginManager(app)
+Migrate(app, db)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,32 @@
+from flask import render_template, redirect, flash, url_for
+from flask_login import login_user, logout_user, current_user
 
+from app import app
+from app.models import User
+from forms import LoginForm
+
+
+@app.route('/')
+def hello():
+    return render_template('base.html', title='Home')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(flask.url_for('hello'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect('/')
+    return render_template('login.html', title='Sign-In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('hello'))
