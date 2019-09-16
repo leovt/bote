1815e3ed35c5bf779b5840e958c6c6639f23fa99
leovt <leonhard.vogt@gmx.ch>
2019-09-16 16:01:50 +0200

    Implement DeclareBlockers

--- client/game.js
+++ client/game.js
@@ -74,13 +74,44 @@ function build_question_ui(event){
       choices.appendChild(document.createElement('br'));
     }
   }
+  else if (question.question == 'DeclareBlockers') {
+    choices.innerHTML = "";
+    for (var action in question.choices) {
+      choices.appendChild(document.createTextNode(question.choices[action].candidate));
+      choices.appendChild(make_input_with_label(
+        type = 'radio',
+        value = 'noblock',
+        name = 'cand-' + action,
+        label = 'Do not block',
+        checked = true
+      ));
+      choices.appendChild(document.createTextNode(' or block one of: '));
+      for (var attacker in question.choices[action].attackers){
+        choices.appendChild(make_input_with_label(
+          type = 'radio',
+          value = attacker,
+          name = 'cand-' + action,
+          label = question.choices[action].attackers[attacker],
+          checked = false
+        ));
+      }
+      choices.appendChild(document.createElement('br'));
+    }
+  }
+  else {
+    choices.innerHTML = "";
+    choices.appendChild(document.createTextNode(JSON.stringify(question)));
+  }
 }
 
 function send_answer () {
   var httpRequest = new XMLHttpRequest();
   httpRequest.open("POST", `${game_uri}/answer`);
   httpRequest.setRequestHeader('Content-Type', 'application/json');
-  httpRequest.addEventListener("load", log_refresh);
+  httpRequest.addEventListener("load", function(){
+    document.getElementById('answer').setAttribute('style', 'display: none;');
+    log_refresh();
+  });
   var answer;
   if (question.question == 'ChooseAction'){
     var radios = document.getElementsByName('action');
@@ -100,6 +131,18 @@ function send_answer () {
       }
     }
   }
+  if (question.question == 'DeclareBlockers'){
+    answer = {};
+    for (var action in question.choices) {
+      var attackers = document.getElementsByName('cand-' + action);
+      for (var i = 0, length = attackers.length; i < length; i++) {
+        if (attackers[i].checked) {
+          if (attackers[i].value != 'noblock')
+            answer[+action] = +attackers[i].value;
+          break;
+        }
+      }
+    }
+  }
   httpRequest.send(JSON.stringify({"answer": answer}));
-  document.getElementById('answer').setAttribute('style', 'display: none;');
 }
