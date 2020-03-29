4d84a717ec78a663f0666a71f299c75196996c4e
leovt <leonhard.vogt@gmx.ch>
2020-03-29 11:28:16 +0200

    primitive chat system for lobby

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
-from flask import render_template, redirect, flash, url_for
-from flask_login import login_user, logout_user, current_user
+from flask import render_template, redirect, flash, url_for, request, abort, jsonify
+from flask_login import login_user, logout_user, current_user, login_required
 
 from app import app
 from app.models import User
@@ -34,4 +34,27 @@ def logout():
 
 @app.route('/')
 def lobby():
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
     return render_template('lobby.html')
+
+global_chat_messages = []
+
+@app.route('/chat_msg', methods=['POST'])
+@login_required
+def send_msg():
+    global_chat_messages.append((current_user.username, request.data.decode('utf8')))
+    return ('', 204)
+
+@app.route('/chat_msg', methods=['GET'])
+@login_required
+def chat():
+    try:
+        first = int(request.args.get('first', 0))
+    except ValueError:
+        abort(400)
+    if first < 0:
+        abort(400)
+
+    return jsonify([{'index': index, 'user': user, 'message': message}
+        for index, (user, message) in enumerate(global_chat_messages[first:], first)])
--- client/lobby.js
+++ client/lobby.js
@@ -1,4 +1,35 @@
 function send_msg() {
   let chat_msg = document.getElementById('chat_msg');
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.open("POST", `/chat_msg`);
+  httpRequest.send(chat_msg.value);
   chat_msg.value = '';
 }
+
+msg_count = 0;
+
+function read_msg() {
+  var chat = document.getElementById('chat');
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.addEventListener("load", function () {
+    result = JSON.parse(httpRequest.responseText);
+    result.forEach(function(msg) {
+      msg_count += 1;
+      let element = document.createElement('li');
+      let user = document.createElement('div');
+      user.innerText = msg.user;
+      user.className = 'chat_user';
+      let message = document.createElement('div');
+      message.innerText = msg.message;
+      message.className = 'chat_message';
+      element.appendChild(user);
+      element.appendChild(message);
+      chat.appendChild(element);
+    });
+  });
+  httpRequest.open("GET", `/chat_msg?first=${msg_count}`);
+  httpRequest.send();
+  window.setTimeout(read_msg, 1000);
+}
+
+read_msg();
